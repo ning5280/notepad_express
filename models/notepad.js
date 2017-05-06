01456c95912ebf282d11d7c53b14c5555e2a3bd3
wangning <529023364@qq.com>
2017-05-06 17:49:11 +0800

    修改文章

--- models/notepad.js
+++ models/notepad.js
@@ -6,10 +6,11 @@ var notepadSchema = new mongoose.Schema({
 	"title":String,//标题
 	"create_time":String,
 	"content":String, //创建时间
+	"username":String
 
 });
 //索引
-notepadSchema.index({ "title": 1});
+notepadSchema.index({ "title": 1,"username":1});
 
 //model
 var notepads = mongoose.model("notepad",notepadSchema);
--- routes/notepad.js
+++ routes/notepad.js
@@ -23,9 +23,13 @@ router.get('/add', function(req, res, next) {
 });
 
 router.post('/add', function(req, res, next) {
+	if(!req.session.userInfo){
+		return res.send('请先登陆');
+	}
 	var title = req.body.title;
 	var content = req.body.content;
-	notepadModel.create({title:title,content:content,create_time:new Date()},function(err){
+	var username = req.body.username;
+	notepadModel.create({title:title,content:content,username:username,create_time:new Date()},function(err){
 		if(err){return res.send(err);}
 		return res.send('添加成功');
 	}) 
@@ -41,4 +45,35 @@ router.get('/del/:id', function(req, res, next) {
     });
  
 });
+
+router.get('/edit/:id', function(req, res, next) {
+	if(!req.session.userInfo){
+		return res.send('请先登陆');
+	}
+	var id = req.params.id;
+	var username = req.session.username;
+	 notepadModel.find({username:username,_id:id},function(err,result){
+	 	if(!result[0]){return res.send('没有该文章');}
+	 	console.log(result);
+	 	return res.render('editNotepad',{"notepadInfo":result[0]});
+	 })
+ 
+});
+
+router.post('/edit/:id', function(req, res, next) {
+	if(!req.session.userInfo){
+		return res.send('请先登陆');
+	}
+	var title = req.body.title;
+	var content = req.body.content;
+	var username = req.body.username;
+	var _id = req.body._id;
+	notepadModel.update({_id:_id},{title:title,content:content,username:username,create_time:new Date()},function(err){
+		if(err){return res.send(err);}
+		return res.send('修改成功');
+	}) 
+});
+
+
+
 module.exports = router;
